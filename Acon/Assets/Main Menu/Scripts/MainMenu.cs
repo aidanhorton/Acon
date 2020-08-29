6415cb4ad9615af13c8c9746d1b5c697c80daac6
Aidan Horton <aidan.horton@virginmedia.com>
2020-08-29 20:21:27 +0100

    Add Unity solution to repository

---
+++ Acon/Assets/Main Menu/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject HomePage;
+    public GameObject PlayPage;
+    public GameObject OptionsPage;
+
+    public void Start()
+    {
+        this.HomePage.SetActive(true);
+        this.PlayPage.SetActive(false);
+        this.OptionsPage.SetActive(false);
+    }
+
+    public void Play()
+    {
+        this.HomePage.SetActive(false);
+        this.PlayPage.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void OpenOptions()
+    {
+        this.HomePage.SetActive(false);
+        this.OptionsPage.SetActive(true);
+    }
+}
